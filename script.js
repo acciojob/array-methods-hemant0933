692d3ea8b6677c38fd8e9e6a99d7b11b1eebec21
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-07-01 21:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 Array.prototype.myMap = function(callback) {
-    
+     let result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
   };
   
   Array.prototype.myFilter = function(callback) {
-  
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
   };
   
   Array.prototype.myReduce = function(callback, initialValue) {
-
+ let accumulator = initialValue === undefined ? this[0] : initialValue;
+  let startIndex = initialValue === undefined ? 1 : 0;
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+  return accumulator;
   };
   
   
